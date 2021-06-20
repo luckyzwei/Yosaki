39aa67f141fa627fe6c0a45948316dd0358a4ab7
PYJ3793 <pyj3793@gmail.com>
2021-06-21 02:33:47 +0900

    1.스킬 자동사용

---
+++ Assets/UiSkillInputButton.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[Serializable]
+public class MyEvent : UnityEvent<int> { }
+
+public class UiSkillInputButton : MonoBehaviour
+{
+    private Coroutine autoUpRoutine;
+
+    private WaitForSeconds autuUpDelay = new WaitForSeconds(0.01f);
+
+    public MyEvent useSkillFunc;
+    public int slotIdx;
+
+    public void PointerDown()
+    {
+        if (autoUpRoutine != null)
+        {
+            CoroutineExecuter.Instance.StopCoroutine(autoUpRoutine);
+        }
+
+        autoUpRoutine = CoroutineExecuter.Instance.StartCoroutine(AutuUpgradeRoutine());
+
+    }
+    public void PointerUp()
+    {
+        if (autoUpRoutine != null)
+        {
+            CoroutineExecuter.Instance.StopCoroutine(autoUpRoutine);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (autoUpRoutine != null)
+        {
+            StopCoroutine(autoUpRoutine);
+        }
+    }
+
+    private IEnumerator AutuUpgradeRoutine()
+    {
+        while (true)
+        {
+            useSkillFunc.Invoke(slotIdx);
+            yield return autuUpDelay;
+        }
+    }
+}
