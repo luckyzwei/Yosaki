54bab80bd81b770c3201bd761e2301ea7ab624c2
PYJ3793 <pyj3793@gmail.com>
2021-07-01 08:13:40 +0900

    no message

--- Assets/02.Script/GoogleManager.cs
+++ Assets/02.Script/GoogleManager.cs
@@ -201,7 +201,7 @@ public class GoogleManager : MonoBehaviour
 #if UNITY_EDITOR
        // return "mayotestguest@gmail.com";
        // return "mabw222ei3no24gi233d0312@n3aver.com"; //GM도비
-        return "3133322333233222333321"; //GM도비
+        return "31333223332332223323321"; //GM도비
 #endif
         return ((PlayGamesLocalUser)Social.localUser).Email;
         //return Social.localUser.id;
