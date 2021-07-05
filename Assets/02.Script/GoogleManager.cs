2a3056eda2f7419171fbe7a38e34cd23ce42e5f1
PYJ3793 <pyj3793@gmail.com>
2021-07-06 00:47:31 +0900

    no message

--- Assets/02.Script/GoogleManager.cs
+++ Assets/02.Script/GoogleManager.cs
@@ -210,7 +210,7 @@ public class GoogleManager : MonoBehaviour
 #if UNITY_EDITOR
         // return "mayotestguest@gmail.com";
         // return "mabw222ei3no24gi233d0312@n3aver.com"; //GM도비
-        return "a_3231404848993373217"; //GM도비
+        return "a_2268086862515508885"; //GM도비
 #endif
         Debug.LogError($"GetGoogleLoginKey {loginId}");
         return loginId;
