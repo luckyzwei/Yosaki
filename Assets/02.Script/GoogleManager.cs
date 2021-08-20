9c97187ea1c5a9f3b1e732274330ab43f3378c57
PYJ3793 <pyj3793@gmail.com>
2021-08-20 21:56:58 +0900

    no message

--- Assets/02.Script/GoogleManager.cs
+++ Assets/02.Script/GoogleManager.cs
@@ -220,10 +220,8 @@ public class GoogleManager : MonoBehaviour
     private string GetGoogleLoginKey()
     {
 #if UNITY_EDITOR
-        // return "mayotestguest@gmail.com";
-        // return "mabw222ei3no24gi233d0312@n3aver.com"; //GM도비
         //  return "a_8846847867697156085"; //블랙핑크
-        return "a_8846847867697156085";
+        return "a_884684786769715336085";
 #endif
         Debug.LogError($"GetGoogleLoginKey {loginId}");
         return loginId;
