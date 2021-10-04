9bd8852e7e0ac1c0654d597bfcdb2b5197d70d52
PYJ3793 <pyj3793@gmail.com>
2021-10-05 00:45:04 +0900

    no message

---
+++ Assets/UiCouponButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiCouponButton : MonoBehaviour
+{
+    void Start()
+    {
+        CheckIos();
+    }
+
+    private void CheckIos()
+    {
+#if UNITY_IOS
+        this.gameObject.SetActive(false);
+#endif
+    }
+}
