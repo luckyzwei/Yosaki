6321d563e85a78f4abd43843d2c93561efc6200f
PYJ3793 <pyj3793@gmail.com>
2021-10-18 23:57:23 +0900

    no message

---
+++ Assets/UiAutoBoss.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiAutoBoss : SingletonMono<UiAutoBoss>
+{
+    [SerializeField]
+    private Toggle toggle;
+
+    public static ReactiveProperty<bool> AutoMode = new ReactiveProperty<bool>(false);
+
+    private new void Awake()
+    {
+        base.Awake();
+    }
+
+    public void WhenToggleChanged(bool on)
+    {
+        AutoMode.Value = on;
+    }
+}
