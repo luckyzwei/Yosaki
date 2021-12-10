312797e50e5ed7fcf556e587dacfb086b295539c
PYJ3793 <pyj3793@gmail.com>
2021-12-11 06:34:32 +0900

    no message

---
+++ Assets/UiIosCouponBoard.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiIosCouponBoard : MonoBehaviour
+{
+    [SerializeField]
+    private UiIosCouponCell cellPrefab;
+
+    [SerializeField]
+    private Transform cellParent;
+
+
+    private void Start()
+    {
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        var tableData = TableManager.Instance.IosCoupon.dataArray;
+
+        for (int i = 0; i < tableData.Length; i++)
+        {
+            var cell = Instantiate<UiIosCouponCell>(cellPrefab, cellParent);
+
+            cell.Initialize(tableData[i]);
+        }
+    }
+}
---
+++ Assets/UiIosCouponCell.cs
@@ -0,0 +1,80 @@
+﻿using BackEnd;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UniRx;
+using UnityEngine;
+
+public class UiIosCouponCell : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject rewardButton;
+
+    [SerializeField]
+    private GameObject completeButton;
+
+    [SerializeField]
+    private TextMeshProUGUI description;
+
+    [SerializeField]
+    private TextMeshProUGUI rewardAmount;
+
+    private IosCouponData couponData;
+
+    public void Initialize(IosCouponData couponData)
+    {
+        this.couponData = couponData;
+
+        description.SetText(couponData.Description);
+
+        Item_Type itemType = (Item_Type)couponData.Rewardtype;
+
+        rewardAmount.SetText($"{CommonString.GetItemName(itemType)} {Utils.ConvertBigNum(couponData.Rewardamount)}개");
+
+        Subscribe();
+    }
+    private void Subscribe()
+    {
+        ServerData.etcServerTable.TableDatas[EtcServerTable.iosCoupon].AsObservable().Subscribe(e =>
+        {
+
+            bool rewarded = ServerData.etcServerTable.IosCouponRewarded(couponData.Id);
+
+            rewardButton.SetActive(!rewarded);
+
+            completeButton.SetActive(rewarded);
+
+        }).AddTo(this);
+    }
+
+    public void OnClickRewardButton()
+    {
+        bool rewarded = ServerData.etcServerTable.IosCouponRewarded(couponData.Id);
+
+        if (rewarded)
+        {
+            PopupManager.Instance.ShowAlarmMessage("이미 보상을 받았습니다.");
+            return;
+        }
+
+        List<TransactionValue> transactions = new List<TransactionValue>();
+
+        Item_Type itemType = (Item_Type)couponData.Rewardtype;
+
+        float rewardAmount = couponData.Rewardamount;
+
+        ServerData.etcServerTable.TableDatas[EtcServerTable.iosCoupon].Value += $"{BossServerTable.rewardSplit}{couponData.Id}";
+
+        Param rewardParam = new Param();
+
+        rewardParam.Add(EtcServerTable.iosCoupon, ServerData.etcServerTable.TableDatas[EtcServerTable.iosCoupon].Value);
+
+
+        transactions.Add(ServerData.GetItemTypeTransactionValueForAttendance(itemType, (int)rewardAmount));
+        transactions.Add(TransactionValue.SetUpdate(EtcServerTable.tableName, EtcServerTable.Indate, rewardParam));
+
+        PopupManager.Instance.ShowAlarmMessage("보상 획득!");
+
+        ServerData.SendTransaction(transactions);
+    }
+}
