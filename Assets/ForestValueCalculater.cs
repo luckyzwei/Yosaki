5d1334140f1729064a3cb3b0465758f182f54b84
PYJ3793 <pyj3793@gmail.com>
2022-05-29 04:26:36 +0900

    no message

---
+++ Assets/ForestValueCalculater.cs
@@ -0,0 +1,25 @@
+﻿using CodeStage.AntiCheat.ObscuredTypes;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForestValueCalculater : MonoBehaviour
+{
+
+    private ObscuredFloat divideNum = 100f;
+    private ObscuredFloat addValue = 0.07f;
+
+    void Start()
+    {
+        UpdateForestValue();
+    }
+
+    private void UpdateForestValue()
+    {
+        float diveidedValue = (float)ServerData.userInfoTable.TableDatas[UserInfoTable.relicKillCount].Value / divideNum;
+
+        GameBalance.forestValue = 1f + (diveidedValue* addValue);
+
+       // GameBalance.forestValue = 1f;
+    }
+}
---
+++ Assets/RelicPowerDescription.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class RelicPowerDescription : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI description;
+
+    [SerializeField]
+    private TextMeshProUGUI abilValueDescription;
+
+    private void Start()
+    {
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        description.SetText($"영혼의숲 최고점수 100마다\n특수무공 능력치가 증가 합니다.\n(공격력증가,크리티컬데미지)");
+        abilValueDescription.SetText($"{GameBalance.forestValue}배");
+    }
+}
