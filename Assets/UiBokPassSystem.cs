012a9af88dfce45e157cb18baca6320af0ffc71d
PYJ_ROG <wer3799@Naver.com>
2022-06-30 17:19:35 +0900

    no message

---
+++ Assets/UiBokPassSystem.cs
@@ -0,0 +1,24 @@
+﻿using BackEnd;
+using CodeStage.AntiCheat.ObscuredTypes;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Purchasing;
+using UnityEngine.UI;
+
+public class UiBokPassSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
