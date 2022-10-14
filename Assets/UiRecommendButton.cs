eb0f93eaf12f20736d9c810c3ca3c6a5c63c3596
kdd6826 <67967689+kdd6826@users.noreply.github.com>
2022-10-14 19:29:27 +0900

    no message

---
+++ Assets/UiRecommendButton.cs
@@ -0,0 +1,22 @@
+﻿using Spine.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiRecommendButton : MonoBehaviour
+{
+    
+    
+
+    [SerializeField]
+    private TextMeshProUGUI nickName;
+
+    public void IncreaseRecommendCount()
+    {
+        PartyRaidManager.Instance.NetworkManager.SendRecommend(nickName.text.ToString());
+
+        this.gameObject.SetActive(false);
+    }
+}
