04f3f827d32fdfb116173fa6a870f81e944354c7
kdd6826 <67967689+kdd6826@users.noreply.github.com>
2022-11-02 11:20:04 +0900

    서재 입장 버그 수정, 천계꽃 효과 수정

--- Assets/UiLibraryBoard.cs
+++ Assets/UiLibraryBoard.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class UiLibraryBoard : MonoBehaviour
 {
-    [SerializeField]
-    private float needKillAmount = 1000000;
 
     [SerializeField]
     private UiTwelveBossContentsView twelveBossContentsView;
@@ -20,15 +18,4 @@ public class UiLibraryBoard : MonoBehaviour
         twelveBossContentsView.Initialize(TableManager.Instance.TwelveBossTable.dataArray[57]);
     }
 
-    private void OnEnable()
-    {
-        if (ServerData.userInfoTable.GetTableData(UserInfoTable.gumGiSoulClear).Value < needKillAmount)
-        {
-            PopupManager.Instance.ShowAlarmMessage($"서재 점수 {Utils.ConvertBigNum(needKillAmount)} 이상일때 해금 됩니다.");
-
-            this.gameObject.SetActive(false);
-
-            return;
-        }
-    }
 }
