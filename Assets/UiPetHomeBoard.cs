5093cb48920d7e13cba75c78d3c10a64d645447c
DESKTOP-53VLV6K\wer37 <wer3799@naver.com>
2022-11-09 19:43:47 +0900

    no message

--- Assets/UiPetHomeBoard.cs
+++ Assets/UiPetHomeBoard.cs
@@ -46,8 +46,48 @@ public class UiPetHomeBoard : MonoBehaviour
     {
         abilDescription.SetText("");
 
-        rewardDescription.SetText("");
+        SetRewardText();
 
         petHasCount.SetText($"환수 보유 {PlayerStats.GetPetHomeHasCount()}");
+    }
+
+    private void SetRewardText()
+    {
+        int petHomeHasCount = PlayerStats.GetPetHomeHasCount();
+
+        var tableData = TableManager.Instance.petHome.dataArray;
+
+        Dictionary<Item_Type, float> rewards = new Dictionary<Item_Type, float>();
+
+        for (int i = 0; i < tableData.Length; i++)
+        {
+            if (petHomeHasCount <= i) break;
+
+            Item_Type rewardType = (Item_Type)tableData[i].Rewardtype;
+            float rewardValue = tableData[i].Rewardvalue;
+
+            if (rewards.ContainsKey(rewardType) == false)
+            {
+                rewards.Add(rewardType, 0f);
+            }
+
+            rewards[rewardType] += rewardValue;
+        }
+
+        var e = rewards.GetEnumerator();
+
+        string description = "";
+
+        while (e.MoveNext())
+        {
+            description += $"{CommonString.GetItemName(e.Current.Key)} {Utils.ConvertBigNum(e.Current.Value)}개\n";
+        }
+
+        rewardDescription.SetText(description);
+    }
+
+    public void OnClickRewardButton()
+    {
+
     }
 }
