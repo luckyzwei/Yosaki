205a66ab8038739585bcd762c42cb243ba226e4d
kdd6826 <67967689+kdd6826@users.noreply.github.com>
2022-11-16 18:43:49 +0900

    여우패스 제목, 기능 수정

--- Assets/UiLevelPassCell.cs
+++ Assets/UiLevelPassCell.cs
@@ -269,6 +269,10 @@ public class UiLevelPassCell : MonoBehaviour
             else if (passInfo.passGrade == 25)
             {
                 PopupManager.Instance.ShowAlarmMessage("여우패스26가 필요합니다!");
+            }  
+            else if (passInfo.passGrade == 26)
+            {
+                PopupManager.Instance.ShowAlarmMessage("여우패스27가 필요합니다!");
             }
 
             return;
@@ -390,6 +394,10 @@ public class UiLevelPassCell : MonoBehaviour
         else if (passInfo.passGrade == 25)
         {
             return ServerData.iapServerTable.TableDatas["levelpass26"].buyCount.Value > 0;
+        }
+        else if (passInfo.passGrade == 26)
+        {
+            return ServerData.iapServerTable.TableDatas["levelpass27"].buyCount.Value > 0;
         }
 
         return hasIapProduct;
